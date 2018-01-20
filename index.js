8a2cdf03b1be153b69bc0af090f7621052b5706b
Yan Hankin <hankinyb@gmail.com>
2018-01-20 19:03:57 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 const app = "I don't do much."
+var kittens = ["Milo", "Otis", "Garfield"]
+
+function destructivelyAppendKitten(name) {
+  kittens.push(name)
+}
+
+function destructivelyPrependKitten(name) {
+  kittens.unshift(name)
+}
+
+function destructivelyRemoveLastKitten(array) {
+  kittens.pop()
+}
+
+function destructivelyRemoveFirstKitten(array) {
+  kittens.shift()
+}
+
+function appendKitten(name) {
+  var kittens2 = [...kittens, name]
+  return kittens2
+}
+
+function prependKitten(name) {
+  var kittens3 = [name, ...kittens]
+  return kittens3
+}
+
+function removeLastKitten(kittens) {
+  kittens4 = kittens.slice(1,3)
+  return kittens4
+}
